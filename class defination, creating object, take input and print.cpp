5df5458a428e0aec40b05d72fc19fe009381d696
Faysal Wahid <158222941+Faysalwahid@users.noreply.github.com>
2024-11-27 11:38:42 +0100

    Add files via upload

---
+++ class defination, creating object, take input and print.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+#include <string>
+#include<iomanip>
+using namespace std;
+
+class Student {
+    public:
+    string name;
+    int rollNumber;
+    float cgpa;
+};
+
+int main(){
+    // Declaring class variables
+    Student student1;
+
+    // Getting input for student1
+    cout << "Enter Name for student 1: ";
+    cin.sync(); // Clear the input buffer properly before getline()
+    getline(cin, student1. name); // Use getline() to capture full name with spaces
+
+    cout << "Enter roll number for student 1: ";
+    cin >> student1. rollNumber;
+
+    cout << "Enter Marks for student 1: ";
+    cin >> student1. cgpa;
+
+
+    // Displaying student 1 details
+    cout << "\nStudent 1 Details :\n";
+    cout << "Name: " << student1. name << '\n';
+    cout << "Roll Number: " << student1. rollNumber << '\n';
+    cout << "CGPA: " << fixed << setprecision(2) << student1. cgpa << endl;
+
+    return 0;
+}
+
